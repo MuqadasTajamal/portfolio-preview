d0bc0d48805e37de8a2a256a0ff57faddd725aee
Muqadas Tajamal <muqadasdevsinn@gmail.com>
2025-09-05 04:53:56 +0500

    add

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3514556066"
+    serviceWorkerVersion: "3162334948"
   }
 });
